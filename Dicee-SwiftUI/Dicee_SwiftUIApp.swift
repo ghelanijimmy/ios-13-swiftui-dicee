91767498c982749aa6b40b5b80d7c3c2f7de9d3c
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2020-07-21 07:19:08 -0400

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Jimmy Ghelani on 2020-07-21.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
